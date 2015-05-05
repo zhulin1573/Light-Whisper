929b51056c0daac6a3f9960873d24b61dfd373e6
zhulin1573 <root@ubuntu.(none)>
2015-05-05 10:36:47 -0700

    something

--- cloud/main.js
+++ cloud/main.js
@@ -1,5 +1,5 @@
 // Use AV.Cloud.define to define as many cloud functions as you want.
 // For example:
 AV.Cloud.define("hello", function(request, response) {
-  response.success("Hello world!");
+  response.success("Hello world wen mao yang!");
 });
